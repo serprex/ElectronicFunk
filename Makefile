3ddb633675947691a1fac963ed24f70a89ecc3c4
Demur Rumed <gunkmute@gmail.com>
2011-09-06 15:33:17 +0000

    Removing text and such. Kae's arrived. Plans: Robotic planet. Frozen aliens. Laser venus fly traps. Warring ideologies of what to do with the player (freeze, vaporize, dissect, plant, etc)

--- Makefile
+++ Makefile
@@ -2,6 +2,5 @@ CC=gcc -std=gnu99 -O3 -s
 O=tgen.o two.o
 all: ${O}
 	${CC} ${O} -o Two -fwhole-program -lglfw -lGL
-spr:
-	pinch font G _ r
-.PHONY:spr
+spr: spr/*
+	pinch font G _ r +2
--- two.c
+++ two.c
@@ -14,11 +14,11 @@
 GLuint Ts[TLEN];
 int Tbg;
 
-void bindTex2d(GLuint i){
+void bindTex(GLuint i){
 	glBindTexture(GL_TEXTURE_2D,Ts[i]);
 }
-int bindTex(int id,const void*px,int w,int h,int f){
-	bindTex2d(id);
+int makeTex(int id,const void*px,int w,int h,int f){
+	bindTex(id);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
@@ -34,16 +34,11 @@ void Tfontxy(char c,float*x,float*y){
 		XY(c-'a'&7,3+(c-'a'>>3))
 	if(c>='A'&&c<='Z')
 		XY(c-'A'&7,3+(c-'A'>>3))
-	switch(c){
-	case'"':TXY('\'',4,0)
-	TXY('%',5,0)
-	TXY('&',6,0)
-	TXY('?',7,0)
-	TXY('/',4,1)
-	TXY('(',5,1)
-	TXY(')',6,1)
-	case'.':TXY(',',7,1)
-	}
+	if(c=='"')c='\'';
+	else if(c==',')c='.';
+	for(int i=0;i<8;i++)
+		if(c=="'%&?/()."[i])
+			XY(4+(i&3),(i&4)>>2)
 }
 
 void drawRect_(int x,int y,int w,int h,float tx,float ty,float tw,float th){
@@ -57,7 +52,8 @@ void drawRect_(int x,int y,int w,int h,float tx,float ty,float tw,float th){
 	glVertex2i(x,y+h);
 }
 
-void drawRect(int x,int y,int w,int h,float tx,float ty,float tw,float th){
+void drawRect(int i,int x,int y,int w,int h,float tx,float ty,float tw,float th){
+	bindTex(i);
 	glBegin(GL_QUADS);
 	drawRect_(x,y,w,h,tx,ty,tw,th);
 	glEnd();
@@ -66,17 +62,15 @@ void drawRect(int x,int y,int w,int h,float tx,float ty,float tw,float th){
 void drawText(int x,int y,char*s){
 	int xx=x;
 	glBegin(GL_QUADS);
-	for(;*s&&y<512;s++){
-		if(!isspace(*s)&&y>-8){
+	for(;*s;s++){
+		if(!isspace(*s)){
 			float tx,ty;
 			Tfontxy(*s,&tx,&ty);
-			if(*s=='i')
-				printf("%f %f\n",tx,ty);
 			drawRect_(x,y,8,8,tx,ty,1./8,1./8);
 		}
 		if(*s=='\n'||x>=376){
 			x=xx;
-			y+=8;
+			if((y+=8)>512)break;
 		}else x+=8;
 	}
 	glEnd();
@@ -84,39 +78,36 @@ void drawText(int x,int y,char*s){
 
 int main(int argc,char**argv){
 	glfwInit();
-	srand(glfwGetTime()*10e5);
+	glfwDisable(GLFW_AUTO_POLL_EVENTS);
 	glfwOpenWindow(384,512,0,0,0,0,0,0,GLFW_WINDOW);
 	glOrtho(0,384,512,0,1,-1);
-	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 	glGenTextures(TLEN,Ts);
-	bindTex(fontT,font,fontW,fontH,fontF);
+	makeTex(fontT,font,fontW,fontH,fontF);
 	Tbg=-1;
 	int t=0;
+	srand(glfwGetTime()*10e5);
 	for(;;){
 		glClear(GL_COLOR_BUFFER_BIT);
 		t++;
 		if(t>>11!=Tbg){
 			Tbg=t>>11;
-			bindTex(_T,_+Tbg*196608,256,256,_F);
-			bindTex(_T+1,_+(Tbg+1)*196608,256,256,_F);
-			bindTex(_T+2,_+(Tbg+2)*196608,256,256,_F);
+			makeTex(_T,_+Tbg*196608,256,256,_F);
+			makeTex(_T+1,_+(Tbg+1)*196608,256,256,_F);
+			makeTex(_T+2,_+(Tbg+2)*196608,256,256,_F);
 		}
-		bindTex2d(_T);
-		drawRect(0,-(t/8&255),256,256,0,0,1,1);
-		bindTex2d(_T+1);
-		drawRect(0,-(t/8&255)+256,256,256,0,0,1,1);
-		bindTex2d(_T+2);
-		drawRect(0,-(t/8&255)+512,256,256,0,0,1,1);
-		bindTex2d(fontT);
-		drawText(256,-t/32,"\n\n\n\nasdfghjklmnopqrstuvwxyz0123456789:;QWERTYUIOPASDFGHJKLZXCVBNM?'%&(.)/");
-		double gT=1./60-glfwGetTime();
+		drawRect(_T,0,-(t/8&255),256,256,0,0,1,1);
+		drawRect(_T+1,0,-(t/8&255)+256,256,256,0,0,1,1);
+		drawRect(_T+2,0,-(t/8&255)+512,256,256,0,0,1,1);
+		bindTex(fontT);
+		glfwSwapBuffers();
+		double gT=1./59-glfwGetTime();
 		if(gT>0)glfwSleep(gT);
 		else printf("%f\n",-gT);
 		glfwSetTime(0);
-		glfwSwapBuffers();
+		glfwPollEvents();
 		if(glfwGetKey(GLFW_KEY_ESC)||!glfwGetWindowParam(GLFW_OPENED))return 0;
 	}
 }
